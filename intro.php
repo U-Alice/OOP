d047b7da365bc619189b0d9817919e15c03c3cae
u-Alice <umugwanezaalice22@gmail.com>
2022-06-13 07:23:08 +0200

    intro to oop

---
+++ intro.php
@@ -0,0 +1,45 @@
+<?php
+
+class Person{
+    public $name;
+   public function Speak(){
+        echo 'Hi '. $this->name;
+    }
+}
+
+$p1 = new Person;
+$p1->name = "John";
+$p1->speak();
+
+class Dog{
+    public $legs = 4;
+    public function display(){
+        echo $this-> legs;
+    }
+}
+$d1 = new Dog();
+$d1->display();
+$d2 = new Dog();
+$d2->legs = 2;
+$d2->display();
+$d3 = new Dog();
+$d3->display();
+$d4 = new Dog();
+$d4-> legs = 3;
+$d4-> display();
+
+//accessing dateTime 
+
+echo "<br>";
+echo((new DateTime())->format('Y')); // Year
+echo((new DateTime())->format('M')); // month
+echo ((new DateTime())->format('D')); // day
+echo ((new DateTime())->format('H')); // hour
+echo ((new DateTime())->format('i')); // minute
+echo ((new DateTime())->format('s')); // second
+echo ((new DateTime())->format('u')); // microsecond
+echo ((new DateTime())->format('i')); //current minutes
+
+
+echo 
+?>
